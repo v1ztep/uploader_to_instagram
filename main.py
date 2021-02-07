73ab0719e73ea2b600d47c03c125108a29344caa
Vladimir <v1ztep@gmail.com>
2021-02-07 12:05:15 +0300

    Сменил директорию на images

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import os
 def main():
     filename = 'hubble.jpeg'
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    dest_folder = 'media'
+    dest_folder = 'images'
 
     response = requests.get(url)
     response.raise_for_status()
 
     os.makedirs(dest_folder, exist_ok=True)
-    with open(f'media/{filename}', 'wb') as file:
+    with open(f'{dest_folder}/{filename}', 'wb') as file:
         file.write(response.content)
 
 
